33edffbed97df93e39662c521bbed5e86e567aec
Célestin <intelce.developpement@gmail.com>
2021-06-14 15:15:26 +0200

    Add templates and style

--- passtock/passtock/settings.py
+++ passtock/passtock/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
+    'password.apps.PasswordConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
